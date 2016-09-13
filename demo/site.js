9cc0820eb35618cb82f51a0b8da0c3cd8c54eca7
Bill Parrott <bill@chimericdream.com>
2016-09-12 21:48:14 -0500

    working on demo

--- demo/site.js
+++ demo/site.js
@@ -5,6 +5,7 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const sass = require('express-compile-sass');
+const render = require('../src/templates/handlebars/render-helper');
 
 const DEFAULT_HTTP_PORT = 8989;
 
@@ -13,25 +14,34 @@ require('../src/templates/handlebars/hbs-helpers');
 
 const app = express();
 app.use(sass({
-    'root': __dirname,
+    'root': path.join(__dirname, '../src/scss'),
     'sourceMap': false,
     'sourceComments': false
 }));
+
 app.use(
     '/bower',
     express.static(path.join(__dirname, '../src/bower'))
 );
-app.use(
-    '/assets/scss',
-    express.static(path.join(__dirname, '../src/scss'))
-);
+
 app.use(
     '/assets/js',
     express.static(path.join(__dirname, '../src/js'))
 );
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '../src/templates/handlebars/pages'));
 hbs.registerPartials(path.join(__dirname, '../src/templates/handlebars/partials'));
+
+app.get('/*', (request, response) => {
+    let template;
+    if (request.path === '/') {
+        template = 'index.hbs';
+    } else {
+        template = `${ request.path }.json`;
+    }
+    render(template, request, response);
+});
 
 app.listen(DEFAULT_HTTP_PORT, () => {
     console.log('web server started!');
--- src/templates/handlebars/render-helper.js
+++ src/templates/handlebars/render-helper.js
@@ -1,9 +1,10 @@
 const _ = require('lodash');
 const pages = require('../../../demo/_data/pages.json');
 
-module.exports = function(template, response, data = {}) {
+module.exports = function(template, request, response, data = {}) {
     _.merge(data, {
-        'demo-pages': pages
+        'demo-pages': pages,
+        'current-page': request.path
     });
     response.render(template, data);
 };
